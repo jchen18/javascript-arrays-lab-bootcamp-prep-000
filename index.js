70aadb9befc93e1d0a34f8548e7984e1df30fec7
Jessie Chen <jessiechen0513@gmail.com>
2018-01-09 15:25:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+kittens = ["Milo", "Otis","Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  return kittens.concat(name)
+}
+
+function prependKitten(name){
+  return kittens.slice(0,0,name)
+}
+
+function removeLastKitten(){
+  return kittens.pop()
+}
+
